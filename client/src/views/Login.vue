493a19730eff0144b9e3aa966709c33fd948e62e
Kim Tae Hoon <eszqsc112@gmail.com>
2020-06-09 20:13:12 +0900

    before commit bootstrap-vue plugin install #28

---
+++ client/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-form-input type="email" v-model="email" placeholder="이메일"></b-form-input>
+    <b-form-input type="password" v-model="pwd" placeholder="패스워드"></b-form-input>
+    <b-button>로그인</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {},
+  data() {
+    return {
+      email: '',
+      pwd: '',
+    };
+  },
+};
+</script>
+<style></style>
