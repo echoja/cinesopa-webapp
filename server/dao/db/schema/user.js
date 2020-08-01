4ad829f12782e1695108c1bbd9fd44f3d4bbb747
Kim Tae Hoon <eszqsc112@gmail.com>
2020-08-01 10:43:04 +0900

    isCorrectPassword 메소드 살짝 개선

--- server/dao/db/schema/user.js
+++ server/dao/db/schema/user.js
@@ -1,21 +1,22 @@
-const authmap = require('./authmap');
+const authmap = require("./authmap");
 const role = authmap.slice(0, -1);
 
-module.exports = function(mongoose){
+module.exports = function (mongoose) {
   let userSchema = new mongoose.Schema({
     email: String,
     name: String,
     c_date: { type: Date, default: Date.now },
     role: {
-      type:String,
+      type: String,
       enum: role,
-    }
+    },
   });
 
   userSchema.methods.isCorrectPassword = async (pwd) => {
-    login = await mongoose.model('Login').findOne({email: this.email})
-    return pwd === login.pwd
-  }
+    if (pwd === undefined) return false;
+    login = await mongoose.model("Login").findOne({ email: this.email });
+    return pwd === login?.pwd;
+  };
 
   return userSchema;
 };
