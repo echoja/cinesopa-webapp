490c50a40416d3d952bd7ec2a7bc96acef1e1769
Kim Tae Hoon <eszqsc112@gmail.com>
2020-08-21 21:03:42 +0900

    쿠키 maxAge 설정 #172

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const logger = require("morgan");
-const session = require("express-session");
-const passport = require("passport");
-const { localAuthConfig } = require("./loader");
-const { dbServerInit } = require("./db/db-server");
-const config = require("./config");
-const { getRouter } = require("./router");
-const uuidv4 = require("uuid").v4;
-const MongoStore = require("connect-mongo")(session);
+const express = require('express');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const { localAuthConfig } = require('./loader');
+const { dbServerInit } = require('./db/db-server');
+const config = require('./config');
+const { getRouter } = require('./router');
+const uuidv4 = require('uuid').v4;
+const MongoStore = require('connect-mongo')(session);
 
 // running mode check
 console.log(`This server is running in ${process.env.NODE_ENV} mode. `);
@@ -26,24 +26,24 @@ webapp.use(
     secret: config.sessionSecret, // env secret required
     resave: false,
     saveUninitialized: false,
-    //cookie: {secure: true, domain: 'sopaseom.com'}, // send cookies over https
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 14 },
     store: new MongoStore({
       url: config.mongodbUrl, // env required
-      collection: "sessions",
+      collection: 'sessions',
     }), // store
-  })
+  }),
 );
 localAuthConfig();
 webapp.use(passport.initialize()); // passport 구동
 webapp.use(passport.session());
 
 // configuring logger
-webapp.use(logger("combined"));
+webapp.use(logger('combined'));
 
 // router
-webapp.use("/", getRouter(webapp));
+webapp.use('/', getRouter(webapp));
 
-webapp.use("/cinesopa", express.static("dist/cinesopa"));
-webapp.use("/sopaseom", express.static("dist/sopaseom"));
+webapp.use('/cinesopa', express.static('dist/cinesopa'));
+webapp.use('/sopaseom', express.static('dist/sopaseom'));
 
-webapp.listen(4000, () => console.log("soapseom and cinesopa server started!"));
+webapp.listen(4000, () => console.log('soapseom and cinesopa server started!'));
