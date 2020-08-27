6aecba9f6fb6f1744a970e1c8f941e95601a0309
Kim Tae Hoon <eszqsc112@gmail.com>
2020-08-27 09:44:24 +0900

    post 자동으로 m_date 바뀌게 하는 테스트

--- server/test/mongoose.spec.js
+++ server/test/mongoose.spec.js
@@ -71,11 +71,13 @@ describe('mongoose schema and model', function () {
           role: '촬영감독',
         },
       ],
-      companies: [{
-        name: '뒷회사',
-        name_en: 'back company',
-        role: '제작사',
-      }],
+      companies: [
+        {
+          name: '뒷회사',
+          name_en: 'back company',
+          role: '제작사',
+        },
+      ],
       watch_grade: '전체관람가',
       reviews: [
         {
@@ -99,29 +101,97 @@ describe('mongoose schema and model', function () {
 
     it('새 doc을 만들었을 때 search 가 무사히 생겨야 함. (훅에 의해 생성됨.)', async function () {
       const test = await model.Film.create(filmInput);
-      expect(test.search).to.equal('ㅇㅣㄱㅓㅅㅇㅡㄴㅅㅠㅍㅓㅍㅏㅇㅜㅓㅈㅔㅁㅗㄱㅇㅣㄷㅏ.#Thisissuperpowertitle.#ㅎㅏ#ha#ㅎㅗ#gh#ㅎㅔㅎㅔ#hehe');
+      expect(test.search).to.equal(
+        'ㅇㅣㄱㅓㅅㅇㅡㄴㅅㅠㅍㅓㅍㅏㅇㅜㅓㅈㅔㅁㅗㄱㅇㅣㄷㅏ.#Thisissuperpowertitle.#ㅎㅏ#ha#ㅎㅗ#gh#ㅎㅔㅎㅔ#hehe',
+      );
     });
 
     it('새 doc을 만든 후 수정했을 때 search 가 수정되어야 함 (UpdateOne에 의해 수정됨.)', async function () {
       await model.Film.create(filmInput);
-      await model.Film.updateOne({ id: 1 }, { title: '헬로맨~', title_en: 'helloman~' });
+      await model.Film.updateOne(
+        { id: 1 },
+        { title: '헬로맨~', title_en: 'helloman~' },
+      );
 
       const updated = await model.Film.findOne({ id: 1 }).lean().exec();
       // console.log(updated);
-      expect(updated.search).to.equal('ㅎㅔㄹㄹㅗㅁㅐㄴ~#helloman~#ㅎㅏ#ha#ㅎㅗ#gh#ㅎㅔㅎㅔ#hehe');
+      expect(updated.search).to.equal(
+        'ㅎㅔㄹㄹㅗㅁㅐㄴ~#helloman~#ㅎㅏ#ha#ㅎㅗ#gh#ㅎㅔㅎㅔ#hehe',
+      );
     });
   });
 
   describe('Post', function () {
     it('새 doc을 만들었을 때 search가 무사히 생겨야 함', async function () {
-      const doc = await model.Post.create({ title: '안녕하세요', content: '<p style="line-height: 2;">이것은콘텐트 입니당.</p>' });
-      expect(doc.search).to.equal('ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ#ㅇㅣㄱㅓㅅㅇㅡㄴㅋㅗㄴㅌㅔㄴㅌㅡㅇㅣㅂㄴㅣㄷㅏㅇ.');
+      const doc = await model.Post.create({
+        title: '안녕하세요',
+        content: '<p style="line-height: 2;">이것은콘텐트 입니당.</p>',
+      });
+      expect(doc.search).to.equal(
+        'ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ#ㅇㅣㄱㅓㅅㅇㅡㄴㅋㅗㄴㅌㅔㄴㅌㅡㅇㅣㅂㄴㅣㄷㅏㅇ.',
+      );
     });
     it('새 doc을 만든 후 수정했을 때 search 가 수정되어야 함 (UpdateOne에 의해 수정됨.)', async function () {
-      await model.Post.create({ title: '안녕하세요', content: '<p style="line-height: 2;">이것은콘텐트 입니당.</p>' });
-      await model.Post.updateOne({ id: 1 }, { title: '이것은 바뀐 제목이에요~~~!' });
+      await model.Post.create({
+        title: '안녕하세요',
+        content: '<p style="line-height: 2;">이것은콘텐트 입니당.</p>',
+      });
+      await model.Post.updateOne(
+        { id: 1 },
+        { title: '이것은 바뀐 제목이에요~~~!' },
+      );
       const found = (await model.Post.find())[0];
-      expect(found.search).to.equal('ㅇㅣㄱㅓㅅㅇㅡㄴㅂㅏㄲㅜㅣㄴㅈㅔㅁㅗㄱㅇㅣㅇㅔㅇㅛ~~~!#ㅇㅣㄱㅓㅅㅇㅡㄴㅋㅗㄴㅌㅔㄴㅌㅡㅇㅣㅂㄴㅣㄷㅏㅇ.');
+      expect(found.search).to.equal(
+        'ㅇㅣㄱㅓㅅㅇㅡㄴㅂㅏㄲㅜㅣㄴㅈㅔㅁㅗㄱㅇㅣㅇㅔㅇㅛ~~~!#ㅇㅣㄱㅓㅅㅇㅡㄴㅋㅗㄴㅌㅔㄴㅌㅡㅇㅣㅂㄴㅣㄷㅏㅇ.',
+      );
+    });
+    it.only('새 doc을 만든 후 수정했을 때 m_date 가 갱신되어야 함.', function (done) {
+      this.timeout(10000);
+      model.Post.create({
+        title: '안녕하세요',
+        content: '<p style="line-height: 2;">이것은콘텐트 입니당.</p>',
+      })
+        .then(() => {
+          model.Post.findOne({ id: 1 })
+            .lean()
+            .exec()
+            .then((originDoc) => {
+              setTimeout(() => {
+                model.Post.updateOne(
+                  { id: 1 },
+                  { title: '시간이 과연 업데이트 되었을까요?' },
+                )
+                  .exec()
+                  .then(() => {
+                    model.Post.findOne({ id: 1 })
+                      .lean()
+                      .exec()
+                      .then((changedDoc) => {
+                        // console.log(
+                        //   `origin: ${originDoc.m_date}, changed: ${changedDoc.m_date}`,
+                        // );
+                        expect(originDoc.m_date).to.not.equal(
+                          changedDoc.m_date,
+                        );
+                        expect(originDoc.m_date).to.be.lessThan(changedDoc.m_date);
+                        done();
+                      })
+                      .catch((err) => {
+                        done(err);
+                      });
+                  })
+                  .catch((err) => {
+                    done(err);
+                  });
+              }, 1000);
+            })
+            .catch((err) => {
+              done(err);
+            });
+        })
+        .catch((err) => {
+          done(err);
+        });
     });
   });
 });
