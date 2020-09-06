befd9ac0e73585391b559aee89cd1d65629340e5
Kim Tae Hoon <eszqsc112@gmail.com>
2020-09-06 09:23:48 +0900

    vue-page-title 커스텀 Mixin으로 만듬. #225

--- cinesopa/src/plugins/vue-page-title.js
+++ cinesopa/src/plugins/vue-page-title.js
@@ -1,10 +1,28 @@
 import Vue from 'vue';
-import VuePageTitle from 'vue-page-title';
-import router from '../router';
+// import VuePageTitle from 'vue-page-title';
+// import router from '../router';
 
-Vue.use(VuePageTitle, {
-  // prefix: 'My App - ',
+// Vue.use(VuePageTitle, {
+//   // prefix: 'My App - ',
 
-  suffix: '- 영화배급협동조합 씨네소파',
-  router,
-});
+//   suffix: '- 영화배급협동조합 씨네소파',
+//   router,
+// });
+
+const suffix = ' - 영화배급협동조합 씨네소파';
+
+const pageTitle = {
+  created() {
+    const { title } = this.$options;
+    if (typeof title === 'string') {
+      document.title = `${title}${suffix}`;
+    } else if (typeof title === 'function') {
+      // 이것이 사용자 정의 함수에게 완성된 vm을 전달하는 핵심 방법!
+      document.title = `${title.call(this, this)}${suffix}`;
+    } else {
+      //
+    }
+  },
+};
+
+Vue.mixin(pageTitle);
