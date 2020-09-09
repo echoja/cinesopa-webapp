35a66689a1530b2fa5e82e553b77f464255c93f6
Kim Tae Hoon <eszqsc112@gmail.com>
2020-09-09 18:38:15 +0900

    custom scss variables 적용

--- cinesopa/src/plugins/bootstrap-vue.js
+++ cinesopa/src/plugins/bootstrap-vue.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './bootstrap-vue-custom-style.scss';
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
