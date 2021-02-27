1274368993e9f5c6e40a2f9bd5e11b503014a26a
Kim Tae Hoon <eszqsc112@gmail.com>
2021-02-27 20:33:50 +0900

    프로젝트 다듬기

---
+++ server/config/common-example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  gmailEmail: '13241324',
+  gmailPassword: '1234',
+  // mongodbUrl:
+  //   'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+  mongodbUrl:
+    'mongodb://localhost:27017',
+  sessionSecret: '1234',
+  kakaoClientId: '12',
+  kakaoClientSecret: '34',
+  bootpayRestAppID: '56',
+  bootpayRestPvKey: '78',
+};
--- server/db/db-server.js
+++ server/db/db-server.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-// const AutoIncrementFactory = require('mongoose-sequence');
 const { mongodbUrl } = require('../config/common');
-
-//
 
 /**
  *
